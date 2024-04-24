35dbb1a064bb6ca66f1f38f9eec7ffff661e2c73
dhc518 <58282810+dhc518@users.noreply.github.com>
2024-04-24 17:42:45 +0900

    add task

---
+++ Assignment/04/4-1.cpp
@@ -0,0 +1,27 @@
+/*게임공학과 2019180013 두혁찬*/
+/*
+#4 - 1. 정수 공간 5개를 배열로 동적 할당받고, 
+정수를 5개 입력받아 평균을 구하고 출력한 뒤 배열을 소멸시키도록 main 함수를 작성하라.
+
+<실행결과>
+정수 5개 입력 >> 1 2 4 5 10
+평균 4.4
+소스코드 캡쳐(스크립트 상단에 학과, 학번, 이름 입력*
+출력화면 캡쳐
+*/
+#include <iostream>
+using namespace std;
+
+void main() {
+	int array[5];
+	cout << "정수 5개 입력 >> ";
+	for (int i = 0; i < 5; i++) {
+		cin >> array[i];
+	}
+	int average
+	cout << endl<< "평균 "<<
+
+	delete array;
+
+
+}
